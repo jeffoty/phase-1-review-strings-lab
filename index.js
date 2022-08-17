818700c4a7e50c6440fac5489ffd124fb9b624df
Jeff <odhiambo47@gmail.com>
2022-08-18 00:09:27 +0300

    completed assingnment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code in this file!
+let currentUser = "Jeff";
+
+let welcomeMessage = "Welcome to Flatbook,!";
+
+welcomeMessage = "Welcome to Flatbook, !";
+
+welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, !";
+
+excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+
+excitedWelcomeMessage = excitedWelcomeMessage.toUpperCase();
+
+let shortGreeting = "Welcome, !";
+
+let me = currentUser.charAt(0)
+shortGreeting = `Welcome, ${me}!`;
+
+
